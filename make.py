c542d7279a4b3864f76cfdbd64e4cab301282342
KrishnaShukla15 <iamkrish015@gmail.com>
2022-04-20 19:11:02 +0530

    Factorial In Py

--- make.py
+++ make.py
@@ -1,2 +1,8 @@
 print("Hello World")
 print("This a sample program!")
+#program to find factorial of a given number
+n=int(input("Enter any number: "))
+r=1
+for i in range(1,n+1):
+    r*=i
+print("Factorial of ",n,"is ",r)
